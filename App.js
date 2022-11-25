a699f6bd449310197912f063a42ec55a0f7d1077
Michel <106674127+MichelZero@users.noreply.github.com>
2022-11-24 21:48:44 -0300

    up

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.fonteBlue}>IMC</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizonte: 15,
+    backgroundColor: '#05b0ab',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fonteBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
 });
